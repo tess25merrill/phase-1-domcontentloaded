2fc2b895c60eed86ba0e7ba06c9252236963871e
Tess <tess.merrill25@gmail.com>
2023-07-20 16:30:21 -0600

    complete dom event lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener(
+    "DOMContentLoaded", function(e) {
+        let text = document.getElementById("text")
+        text.textContent = 'This is really cool!'
+    })
+    
